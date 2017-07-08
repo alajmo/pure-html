b786edc30313a566eff4712635ddf9a3e624f34e
samiralajmovic <alajmovic.samir@gmail.com>
2017-07-08 03:35:21 +0200

    test: Add tests for folder structure

--- test/test.js
+++ test/test.js
@@ -5,12 +5,14 @@ const fs = require('fs');
 const html5Lint = require('html5-lint');
 const test = require('tap').test;
 
-test('Check that index.html is generated', t => {
+test('Files should be generated', t => {
   child.execSync('node bin/pure-html -c test/files/.purehtmlrc.json', {
     stdio: [0, 1, 2]
   });
   t.equal(fs.existsSync('test/files/dest/index.html'), true, 'File exists.');
-  t.equal(fs.existsSync('test/files/dest/index2.html'), true, 'File exists.');
+  t.equal(fs.existsSync('test/files/dest/index2.html'), true, 'Directory file level 0 test');
+  t.equal(fs.existsSync('test/files/dest/example/index.html'), true, 'Directory file level 1 test');
+  t.equal(fs.existsSync('test/files/dest/example/example-2/index-2.html'), true, 'Directory file level 1 test');
 
   // TODO: Check validitity of html files.
   // const html = fs.readFileSync('test/files/dest/index.html');
@@ -19,7 +21,7 @@ test('Check that index.html is generated', t => {
   cleanUp();
 });
 
-test('Config file exists', (t) => {
+test('Config file should exist', (t) => {
   t.equal(fs.existsSync('config/.purehtmlrc.json'), true, 'default.config.json exists.');
   t.equal(fs.existsSync('config/config.json'), true, 'config.json exists.');
   t.end();
