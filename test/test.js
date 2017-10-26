fef034eebbdaaa250452950e03bb4055ccfa647c
samiralajmovic <alajmovic.samir@gmail.com>
2017-10-26 19:54:47 +0200

    BREAKING CHANGE: Remove possibility to have a pure-html config

--- test/test.js
+++ test/test.js
@@ -1,32 +1,45 @@
 const child = require('child_process');
-const config = require('./files/.purehtmlrc.json');
 const del = require('del');
 const fs = require('fs');
 const html5Lint = require('html5-lint');
 const test = require('tap').test;
 
+const src = 'test/files/src';
+const dest = 'test/files/dist';
+
 test('Files should be generated', t => {
-  child.execSync('node bin/pure-html -c test/files/.purehtmlrc.json', {
+  child.execSync(`node bin/pure-html -s ${src} -d ${dest} -l`, {
     stdio: [0, 1, 2]
   });
-  t.equal(fs.existsSync('test/files/dest/index.html'), true, 'File exists.');
-  t.equal(fs.existsSync('test/files/dest/index2.html'), true, 'Directory file level 0 test');
-  t.equal(fs.existsSync('test/files/dest/example/index.html'), true, 'Directory file level 1 test');
-  t.equal(fs.existsSync('test/files/dest/example/example-2/index-2.html'), true, 'Directory file level 1 test');
+
+  t.equal(fs.existsSync('test/files/dist/index.html'), true, 'File exists.');
+
+  t.equal(
+    fs.existsSync('test/files/dist/index2.html'),
+    true,
+    'Directory file level 0 test'
+  );
+
+  t.equal(
+    fs.existsSync('test/files/dist/example/index.html'),
+    true,
+    'Directory file level 1 test'
+  );
+
+  t.equal(
+    fs.existsSync('test/files/dist/example/example-2/index-2.html'),
+    true,
+    'Directory file level 1 test'
+  );
 
   // TODO: Check validitity of html files.
-  // const html = fs.readFileSync('test/files/dest/index.html');
-  // const html2 = fs.readFileSync('test/files/dest/index2.html');
+  // const html = fs.readFileSync('test/files/dist/index.html');
+  // const html2 = fs.readFileSync('test/files/dist/index2.html');
+
   t.end();
   cleanUp();
 });
 
-test('Config file should exist', (t) => {
-  t.equal(fs.existsSync('config/default.purehtmlrc.json'), true, 'default config exists.');
-  t.equal(fs.existsSync('config/config.json'), true, 'config.json exists.');
-  t.end();
-});
-
 function cleanUp() {
-  del.sync(config.dest);
+  del.sync(dest);
 }
