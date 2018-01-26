36c2228467e3a7d8cb304901156d06033a12e45c
samiralajmovic <alajmovic.samir@gmail.com>
2018-01-26 04:29:19 +0100

    test: Add inline style test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,13 @@
 const child = require('child_process');
+const jsdom = require('jsdom');
+const { promisify } = require('util');
+const { JSDOM } = jsdom;
 const del = require('del');
 const fs = require('fs');
-const html5Lint = require('html5-lint');
 const path = require('path');
 const test = require('tap').test;
+
+const readFile = promisify(fs.readFile);
 
 main();
 
@@ -15,15 +19,31 @@ function main() {
 }
 
 function generateFiles() {
-  test('Files should be generated', t => {
+  test('Html files should be generated', async t => {
+    const { JSDOM } = jsdom;
+
     const src = 'test/files/src';
     const dest = 'test/files/dist';
+
+    let html = await readFile('test/files/src/index.html', 'utf-8');
+    let jsDOM = new JSDOM(html).window;
+    let numStyleElements = jsDOM.document.querySelectorAll('link[inline]');
+    t.equal(
+      Object.keys(numStyleElements).length,
+      2,
+      'Contains 2 link elements with inline attribute'
+    );
 
     child.execSync(`node bin/pure-html -s ${src} -d ${dest} -l`, {
       stdio: [0, 1, 2]
     });
 
     t.equal(fs.existsSync('test/files/dist/index.html'), true, 'File exists.');
+
+    html = await readFile('test/files/dist/index.html', 'utf-8');
+    jsDOM = new JSDOM(html).window;
+    numStyleElements = jsDOM.document.querySelectorAll('style');
+    t.equal(Object.keys(numStyleElements).length, 2, 'Contains 2 inline style elements')
 
     t.equal(
       fs.existsSync('test/files/dist/index2.html'),
@@ -37,10 +57,28 @@ function generateFiles() {
       'Directory file level 1 test'
     );
 
+    html = await readFile('test/files/src/example/example-2/index-2.html', 'utf-8');
+    jsDOM = new JSDOM(html).window;
+    numStyleElements = jsDOM.document.querySelectorAll('link[inline]');
+    t.equal(
+      Object.keys(numStyleElements).length,
+      1,
+      'Contains 1 link elements with inline attribute'
+    );
+
     t.equal(
       fs.existsSync('test/files/dist/example/example-2/index-2.html'),
       true,
       'Directory file level 1 test'
+    );
+
+    html = await readFile('test/files/dist/example/example-2/index-2.html', 'utf-8');
+    jsDOM = new JSDOM(html).window;
+    numStyleElements = jsDOM.document.querySelectorAll('style');
+    t.equal(
+      Object.keys(numStyleElements).length,
+      1,
+       'Contains 1 inline style elements'
     );
 
     t.end();
@@ -49,7 +87,7 @@ function generateFiles() {
 }
 
 function generateFilesPrefixDot() {
-  test('Files should be generated when prefixing path by dot', t => {
+  test('Html files should be generated when prefixing path by dot', t => {
     const src = './test/files/src';
     const dest = './test/files/dist';
 
@@ -83,7 +121,7 @@ function generateFilesPrefixDot() {
 }
 
 function generateFilesAbsolutePath() {
-  test('Files should be generated when using absolute path', t => {
+  test('Html files should be generated when using absolute path', t => {
     const src = path.resolve(path.join(__dirname, './files/src'));
     const dest = path.resolve(path.join(__dirname, './files/dist'));
 
@@ -117,7 +155,7 @@ function generateFilesAbsolutePath() {
 }
 
 function generateAssets() {
-  test('Assets with inline attribute should be inlined', t => {
+  test('css and js files with non-inline attribute should be copied to dest', t => {
     const src = 'test/files/src2';
     const dest = 'test/files/dist2';
 
@@ -143,3 +181,5 @@ function generateAssets() {
     del.sync(dest);
   });
 }
+
+function calcNumberScripts() {}
